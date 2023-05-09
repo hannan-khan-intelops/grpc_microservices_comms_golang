cfdbe250f25e390896983221263688bab5c961b7
Hannan Khan <hkhan@intelops.dev>
2023-05-09 15:50:31 -0500

    updated the server with grpc code

---
+++ service-2-grpc-server/go.mod
@@ -0,0 +1,19 @@
+module example.com/service-2-grpc-server
+
+go 1.20
+
+replace example.com/microservice => ../microservice
+
+require (
+	example.com/microservice v0.0.0-00010101000000-000000000000
+	google.golang.org/grpc v1.55.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ service-2-grpc-server/main.go
@@ -0,0 +1,64 @@
+// __author__ = "Hannan Khan"
+// __credits__ = ["Hannan Khan"]
+// __version__ = "0.0.1"
+// __email__ = "hkhan@intelops.dev"
+// __status__ = "Development"
+
+// This script is the client portion of the grpc microservice. The client will initiate streaming, and the server will
+// stream its data (multiple responses) to the client.
+// Tutorial src: https://www.freecodecamp.org/news/grpc-server-side-streaming-with-go/
+
+package service_2_grpc_server
+
+import (
+	pb "example.com/microservice"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+type server struct{}
+
+func (s server) FetchResponse(in *pb.Request, srv pb.StreamService_FetchResponseServer) error {
+
+	log.Printf("Fetch response for response.id: %d", id)
+
+	// we will be using a waitGroup in order to allow the process to be concurrent.
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(count int64) {
+			defer wg.Done()
+
+			// this sleep function will simulate server process time.
+			time.Sleep(time.Duration(count) * time.Second)
+			resp := pb.Response{Result: fmt.Sprintf("Request #%d for Id: %d", count, in.Id)}
+			if err := srv.Send(&resp); err != nil {
+				log.Printf("Send error %v", err)
+			}
+			log.Printf("Finishing request number: %d", count)
+		}(int64(i))
+	}
+	wg.Wait()
+	return nil
+}
+
+func main() {
+	// create a listener
+	lis, err := net.Listen("tcp", ":50005")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	// create the actual gRPC server
+	s := grpc.NewServer()
+	pb.RegisterStreamServiceServer(s, server{})
+
+	log.Println("Starting server")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
